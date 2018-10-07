12a2e83808e9462dc83dd3dfa50fc62c3bd09d8b
Maksym Zavershynskyi <max@near.ai>
2018-10-06 22:27:29 -0700

    Add standard Migrations files

--- contracts/ETHLock.sol
+++ contracts/ETHLock.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.22;
 
 contract ERC721Receiver {
     bytes4 constant ERC721_RECEIVED = 0xf0b9e5ba;
@@ -6,9 +6,9 @@ contract ERC721Receiver {
 }
 
 contract ETHLock is ERC721Receiver {
-    event Locked(address owner, uint256 tokenId, uint256 data);
+    event Locked(address owner, uint256 tokenId, bytes data);
 
-    function onERC721Received(address owner, uint256 tokenId, uint256 data) public returns(bytes4) {
+    function onERC721Received(address owner, uint256 tokenId, bytes data) public returns(bytes4) {
         emit Locked(owner, tokenId, data);
         return ERC721_RECEIVED;
     }
