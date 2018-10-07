0054dc99f31ce31ad43c234b48e31fffc7e36fdc
Maksym Zavershynskyi <max@near.ai>
2018-10-06 22:58:05 -0700

    Add locking mechanism to ETHLocking

--- contracts/ETHLock.sol
+++ contracts/ETHLock.sol
@@ -6,15 +6,22 @@ contract ETHLock {
 
     bytes4 constant ERC721_RECEIVED = 0xf0b9e5ba;
     event Locked(address owner, uint256 tokenId, bytes data);
+    event Transfer(address sender, uint256 tokenId);
 
     address ethTokenAddress;
 
-    constructor(address _ethTokenAddress) {
+    constructor(address _ethTokenAddress) public {
         ethTokenAddress = _ethTokenAddress;
     }
 
     function onERC721Received(address owner, uint256 tokenId, bytes data) public returns(bytes4) {
         emit Locked(owner, tokenId, data);
         return ERC721_RECEIVED;
+    }
+
+    function lock(uint256 tokenId) public {
+        emit Transfer(msg.sender, tokenId);
+        ETHToken ethToken = ETHToken(ethTokenAddress);
+        ethToken.transferFrom(msg.sender, this, tokenId);
     }
 }
